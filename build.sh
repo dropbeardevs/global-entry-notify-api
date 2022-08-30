c7a6467b5a8b27babd6de41082d4345c9c44546a
Christian Dulu <christian@chdulu.com>
2022-08-29 23:40:32 -0700

    Updated build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#/bin/sh
 
 mkdir ./bin
 
@@ -7,3 +7,5 @@ go build -v -o ./bin ./...
 rm ./bin/grpc_testclient
 
 cp ../secrets/global-entry-notify-api/* ./bin
+
+#docker build -t global-entry-notify-api .
