2334c267dcd8a00914757e1c048a3ef15bbeed11
Christian Dulu <christian@chdulu.com>
2022-08-30 00:07:18 -0700

    Run as background

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker run --rm -p 8080:8080 \
+docker run -d --rm -p 8080:8080 \
 -e GOOGLE_APPLICATION_CREDENTIALS="global-entry-c8373-fe72e1ae9c11.json" \
 -e GLOBAL_ENTRY_NOTIFY_API_CONFIG="config.yml" \
 -v /var/log:/usr/local/bin/logs \
