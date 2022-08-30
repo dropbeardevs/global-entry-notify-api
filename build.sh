332712f941d87d89bbf2ead64486e654a60ca40f
Christian Dulu <christian@chdulu.com>
2022-08-30 00:29:09 -0700

    Updated build

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ go build -v -o ./bin ./...
 
 rm ./bin/grpc_testclient
 
-cp ../secrets/global-entry-notify-api/* ./bin
+cp ../secrets/global-entry-notify-api/global-entry-c8373-fe72e1ae9c11.json ./bin/fbauth.json
+cp ../secrets/global-entry-notify-api/config.prod.yml ./bin/config.yml
 
 docker build -t global-entry-notify-api .
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker run -d --rm -p 8080:8080 \
--e GOOGLE_APPLICATION_CREDENTIALS="global-entry-c8373-fe72e1ae9c11.json" \
+-e GOOGLE_APPLICATION_CREDENTIALS="fbauth.json" \
 -e GLOBAL_ENTRY_NOTIFY_API_CONFIG="config.yml" \
 -v /var/logs/global-entry-notify-api:/var/logs/global-entry-notify-api \
 global-entry-notify-api 
