d37176a19fe7899a2117679fa05d71821fc5a8e6
Christian Dulu <christian@chdulu.com>
2022-08-30 00:06:36 -0700

    Oops

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker run --rm -p 8080:8080 \
 -e GOOGLE_APPLICATION_CREDENTIALS="global-entry-c8373-fe72e1ae9c11.json" \
 -e GLOBAL_ENTRY_NOTIFY_API_CONFIG="config.yml" \
--v /var/log:/usr/local/bin/logs
+-v /var/log:/usr/local/bin/logs \
 global-entry-notify-api 
